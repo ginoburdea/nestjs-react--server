b3c51d7e82a868b28f4a900f93fe7da23e277ecf
Gino Burdea <burdea.gino@gmail.com>
2024-10-10 17:21:41 +0300

    test: add tests for the create user service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,6 +3,7 @@ import { UsersService } from './users.service';
 import { Chance } from 'chance';
 import { PrismaService } from '../common/prisma.service';
 import { expectValidationError } from '../test-utils/expectValidationError';
+import { truncateAllTables } from '../test-utils/truncateAllTables';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -13,6 +14,8 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+
+    await truncateAllTables(service['prisma']);
   });
 
   describe('validateMasterPassword', () => {
@@ -34,6 +37,41 @@ describe('UsersService', () => {
         'masterPassword',
         'INVALID_MASTER_PASSWORD',
         () => service['validateMasterPassword'](masterPassword),
+      );
+    });
+  });
+
+  describe('createUser', () => {
+    it('Should create a user when the email is not in use', async () => {
+      const userData = {
+        name: new Chance().name(),
+        email: new Chance().email(),
+        password: new Chance().string({ length: 16 }),
+      };
+
+      const user = await service['createUser'](userData);
+
+      expect(user).toMatchObject({
+        name: userData.name,
+        email: userData.email,
+      });
+
+      const userFromDb = await service['prisma'].users.findFirst({
+        where: { id: user.id },
+      });
+      expect(userFromDb).not.toBeNull();
+    });
+
+    it('Should throw when the email is already in use', async () => {
+      const userData = {
+        name: new Chance().name(),
+        email: new Chance().email(),
+        password: new Chance().string({ length: 16 }),
+      };
+      await service['createUser'](userData);
+
+      await expectValidationError('email', 'EMAIL_IN_USE', () =>
+        service['createUser'](userData),
       );
     });
   });
