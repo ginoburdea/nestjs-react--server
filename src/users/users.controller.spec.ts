6534ce8b3161db37ef3e71a8c9957599700a7d2e
Gino Burdea <burdea.gino@gmail.com>
2024-10-10 18:35:05 +0300

    test: add unit tests for the register controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,21 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { PrismaService } from '../common/prisma.service';
+
+jest.mock('./users.service');
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, PrismaService],
+      providers: [UsersService],
       controllers: [UsersController],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('register', () => {
+    it('Should call the register user service and the send auth response function', async () => {
+      const register = jest
+        .spyOn(usersService as any, 'register')
+        .mockResolvedValue({});
+      const sendAuthRes = jest
+        .spyOn(controller as any, 'sendAuthRes')
+        .mockResolvedValue(undefined);
+
+      const body = {};
+      const res = {};
+
+      await controller.register(body as any, res as any);
+
+      expect(register).toHaveBeenCalled();
+      expect(sendAuthRes).toHaveBeenCalled();
+    });
   });
 });
