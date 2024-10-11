0f83c511f645c8fa72559b064f80c1d67916e11b
Gino Burdea <burdea.gino@gmail.com>
2024-10-12 00:59:45 +0300

    feat: add login user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { UsersService } from './users.service';
 import { RegisterBody } from './dto/register.dto';
 import { Response } from 'express';
 import { Users } from '@prisma/client';
+import { LoginBody } from './dto/login.dto';
 
 @Controller('users')
 export class UsersController {
@@ -31,6 +32,12 @@ export class UsersController {
   async register(@Body() body: RegisterBody, @Res() res: Response) {
     const { user, token, tokenExpiresAt } =
       await this.usersService.register(body);
+    this.sendAuthRes(res, user, token, tokenExpiresAt);
+  }
+
+  @Post('login')
+  async login(@Body() body: LoginBody, @Res() res: Response) {
+    const { user, token, tokenExpiresAt } = await this.usersService.login(body);
     this.sendAuthRes(res, user, token, tokenExpiresAt);
   }
 }
