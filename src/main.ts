3c9dbf988ac1a29368b937f4f058a488a99e053e
Gino Burdea <burdea.gino@gmail.com>
2024-10-11 18:15:12 +0300

    refactor: extract app loading to a function for reusability

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { CustomExceptionFilter } from './common/custom-exception.filter';
-import { validationPipeConfig } from './config/validationPipe';
+import { loadApp } from './loaders/loadApp';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
-  app.useGlobalFilters(new CustomExceptionFilter());
+  const app = await loadApp();
   await app.listen(3000);
 }
+
 bootstrap();
