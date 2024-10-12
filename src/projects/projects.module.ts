6e51c1051b4b3fb4a8d02e58281d977e105b3237
Gino Burdea <burdea.gino@gmail.com>
2024-10-12 10:51:29 +0300

    feat: add projects service

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../common/prisma.service';
 import { UsersService } from '../users/users.service';
 import { ProjectsController } from './projects.controller';
+import { ProjectsService } from './projects.service';
 
 @Module({
   controllers: [ProjectsController],
-  providers: [PrismaService, UsersService],
+  providers: [PrismaService, UsersService, ProjectsService],
 })
 export class ProjectsModule {}
