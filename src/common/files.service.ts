89a8a94e87c0aac88022db539f8740148b56d6b5
Gino Burdea <burdea.gino@gmail.com>
2024-10-12 13:11:11 +0300

    feat: implement upload local file service

--- src/common/files.service.ts
+++ src/common/files.service.ts
@@ -1,10 +1,17 @@
+import { ensureDir, writeFile } from 'fs-extra';
+import { join, resolve } from 'path';
+
 export abstract class FileService {
   async upload(file: Buffer, name: string, mimeType: string) {}
 }
 
 export class LocalFileService implements FileService {
   async upload(file: Buffer, name: string, mimeType: string) {
-    // TODO: implement this function
+    const folderPath = resolve('.temp/uploads');
+    await ensureDir(folderPath);
+
+    const filePath = join(folderPath, name);
+    await writeFile(filePath, file);
   }
 }
 
