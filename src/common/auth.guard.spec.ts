96b095a81a8d3b5c700332e5b4a917bf37b7c17c
Gino Burdea <burdea.gino@gmail.com>
2024-10-13 14:28:15 +0300

    test: add tests for canActivate auth guard function

--- src/common/auth.guard.spec.ts
+++ src/common/auth.guard.spec.ts
@@ -2,6 +2,10 @@ import { Chance } from 'chance';
 import { PrismaService } from '../common/prisma.service';
 import { truncateAllTables } from '../test-utils/truncateAllTables';
 import { AuthGuard } from './auth.guard';
+import jwt from 'jsonwebtoken';
+import { UnauthorizedException } from '@nestjs/common';
+
+jest.mock('jsonwebtoken');
 
 describe('AuthGuard', () => {
   let authGuard: AuthGuard;
@@ -102,6 +106,88 @@ describe('AuthGuard', () => {
         errors++;
       }
       expect(errors).toEqual(0);
+    });
+  });
+
+  describe('canActivate', () => {
+    it('Should call the this.accessTokenFromCookies, jwt.verify and this.validateUserId functions and return true if there are no errors', async () => {
+      const context = {
+        switchToHttp: jest.fn().mockReturnValue({
+          getRequest: jest.fn().mockReturnValue({}),
+        }),
+      };
+
+      const accessTokenFromCookies = jest
+        .spyOn(authGuard as any, 'accessTokenFromCookies')
+        .mockReturnValue(undefined);
+
+      (jwt.verify as jest.Mock).mockReturnValue({});
+
+      const validateUserId = jest
+        .spyOn(authGuard as any, 'validateUserId')
+        .mockReturnValue(undefined);
+
+      const res = await authGuard['canActivate'](context as any);
+
+      expect(accessTokenFromCookies).toHaveBeenCalled();
+      expect(jwt.verify as jest.Mock).toHaveBeenCalled();
+      expect(validateUserId).toHaveBeenCalled();
+      expect(res).toEqual(true);
+    });
+
+    it('Should throw UnauthorizedException if jwt.verify throws', async () => {
+      const context = {
+        switchToHttp: jest.fn().mockReturnValue({
+          getRequest: jest.fn().mockReturnValue({}),
+        }),
+      };
+
+      const accessTokenFromCookies = jest
+        .spyOn(authGuard as any, 'accessTokenFromCookies')
+        .mockReturnValue(undefined);
+
+      let errors = 0;
+      try {
+        await authGuard['canActivate'](context as any);
+      } catch (error) {
+        errors++;
+        expect(error).toBeInstanceOf(UnauthorizedException);
+      }
+      expect(errors).toEqual(1);
+
+      expect(accessTokenFromCookies).toHaveBeenCalled();
+      expect(jwt.verify as jest.Mock).toHaveBeenCalled();
+    });
+
+    it('Should throw UnauthorizedException if this.validateUserId throws', async () => {
+      const context = {
+        switchToHttp: jest.fn().mockReturnValue({
+          getRequest: jest.fn().mockReturnValue({}),
+        }),
+      };
+
+      const accessTokenFromCookies = jest
+        .spyOn(authGuard as any, 'accessTokenFromCookies')
+        .mockReturnValue(undefined);
+
+      (jwt.verify as jest.Mock).mockReturnValue({});
+
+      const validateUserId = jest
+        .spyOn(authGuard as any, 'validateUserId')
+        .mockRejectedValue(new Error('lorem ipsum'));
+
+      let errors = 0;
+      try {
+        await authGuard['canActivate'](context as any);
+      } catch (error) {
+        errors++;
+        expect(error).toBeInstanceOf(UnauthorizedException);
+      }
+      expect(errors).toEqual(1);
+
+      expect(accessTokenFromCookies).toHaveBeenCalled();
+      expect(jwt.verify as jest.Mock).toHaveBeenCalled();
+      expect(validateUserId).toHaveBeenCalled();
     });
   });
 });
