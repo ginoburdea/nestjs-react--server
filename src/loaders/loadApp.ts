111f37d7e99ae68b23fc9bd46f77439c4d001cd4
Gino Burdea <burdea.gino@gmail.com>
2024-10-13 13:51:55 +0300

    feat: add auth guard

--- src/loaders/loadApp.ts
+++ src/loaders/loadApp.ts
@@ -3,12 +3,14 @@ import { AppModule } from '../app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { CustomExceptionFilter } from '../common/custom-exception.filter';
 import { validationPipeConfig } from '../config/validationPipe';
+import cookieParser from 'cookie-parser';
 
 export const loadApp = async () => {
   const server = await NestFactory.create(AppModule);
 
   server.useGlobalPipes(new ValidationPipe(validationPipeConfig));
   server.useGlobalFilters(new CustomExceptionFilter());
+  server.use(cookieParser());
 
   if (process.env.NODE_ENV !== 'production') {
     server.enableCors({ origin: '*' });
