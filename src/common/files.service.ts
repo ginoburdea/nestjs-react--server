be0a3d30f3d0b788ac65f1612254464c3a7202a9
Gino Burdea <burdea.gino@gmail.com>
2024-10-13 12:32:42 +0300

    feat: add upload s3 file service

--- src/common/files.service.ts
+++ src/common/files.service.ts
@@ -1,5 +1,6 @@
 import { ensureDir, writeFile } from 'fs-extra';
 import { join, resolve } from 'path';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 export abstract class FileService {
   async upload(file: Buffer, name: string, mimeType: string) {}
@@ -16,7 +17,27 @@ export class LocalFileService implements FileService {
 }
 
 export class S3FileService implements FileService {
+  private s3Client: S3Client;
+
+  constructor() {
+    this.s3Client = new S3Client({
+      endpoint: process.env.S3_ENDPOINT,
+      region: process.env.S3_REGION,
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      },
+    });
+  }
+
   async upload(file: Buffer, name: string, mimeType: string) {
-    // TODO: implement this function
+    const uploadFileCommand = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET,
+      Body: file,
+      ContentType: mimeType,
+      Key: name,
+    });
+
+    await this.s3Client.send(uploadFileCommand);
   }
 }
