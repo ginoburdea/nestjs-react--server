ad9b016adc6e24ce4b018d25741d02e6e3e17a47
Gino Burdea <burdea.gino@gmail.com>
2024-10-13 19:31:00 +0300

    test: add e2e tests for the create projects route

---
+++ test/projects.e2e-spec.ts
@@ -0,0 +1,70 @@
+import request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { loadApp } from '../src/loaders/loadApp';
+import { Chance } from 'chance';
+import { Server } from 'http';
+import { UsersService } from '../src/users/users.service';
+import { resolve } from 'path';
+import { PrismaService } from '../src/common/prisma.service';
+
+describe('/projects', () => {
+  let app: INestApplication;
+  let server: Server;
+
+  beforeAll(async () => {
+    app = await loadApp();
+    server = app.getHttpServer();
+
+    await app.listen(3000);
+  }, 10000);
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe(`POST /`, () => {
+    it('Should create a project', async () => {
+      const usersService = app.get(UsersService);
+      const { token } = await usersService.register({
+        name: new Chance().name(),
+        email: new Chance().email().toLowerCase(),
+        password: new Chance().string({ length: 16 }),
+        masterPassword: process.env.MASTER_PASSWORD,
+      });
+
+      const filePath = resolve('test/data/photo.png');
+
+      const project = {
+        name: new Chance().string({ length: 32 }),
+        url: new Chance().url(),
+        description: new Chance().string({ length: 128 }),
+      };
+
+      const res = await request(server)
+        .post('/projects')
+        .attach('photos', filePath)
+        .attach('photos', filePath)
+        .attach('photos', filePath)
+        .field('name', project.name)
+        .field('url', project.url)
+        .field('description', project.description)
+        .set('Cookie', `access_token=${token}`);
+
+      expect(res.statusCode).toEqual(200);
+      expect(typeof res.body.id).toEqual('number');
+
+      const prisma = app.get(PrismaService);
+      const createdProject = await prisma.projects.findFirst({
+        where: { id: res.body.id },
+        include: { photos: true },
+      });
+
+      expect(createdProject).toMatchObject({
+        name: project.name,
+        url: project.url,
+        description: project.description,
+      });
+      expect(createdProject.photos).toHaveLength(3);
+    });
+  });
+});
