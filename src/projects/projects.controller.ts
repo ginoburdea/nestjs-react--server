9a75a0bcf6a4a8c6ab0cc7977c65b3be9805e584
Gino Burdea <burdea.gino@gmail.com>
2024-10-13 13:08:23 +0300

    feat: add create project controller

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProjectsService } from './projects.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { CreateProjectBody } from './dto/create.dto';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private projectsService: ProjectsService) {}
+
+  @Post('')
+  @HttpCode(200)
+  @UseInterceptors(FilesInterceptor('photos'))
+  async createProject(
+    @UploadedFiles() photos: Express.Multer.File[],
+    @Body() body: CreateProjectBody,
+  ) {
+    const project = await this.projectsService.createProject({
+      ...body,
+      photos: photos.map((photo) => ({
+        content: photo.buffer,
+        mimeType: photo.mimetype,
+      })),
+    });
+
+    return { id: project.id };
+  }
 }
