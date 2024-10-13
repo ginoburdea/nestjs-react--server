f6a53cd3bd184702aee4b844d0caa9cda2c8785c
Gino Burdea <burdea.gino@gmail.com>
2024-10-13 13:06:10 +0300

    feat: add upload photos projects service

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/prisma.service';
 import { FileService } from '../common/files.service';
+import { randomUUID } from 'crypto';
+
+interface File {
+  content: Buffer;
+  mimeType: string;
+}
 
 @Injectable()
 export class ProjectsService {
@@ -8,4 +14,21 @@ export class ProjectsService {
     private prisma: PrismaService,
     private fileService: FileService,
   ) {}
+
+  private async uploadPhotos(projectId: number, photos: File[]) {
+    const photosWithNames = photos.map((photo) => ({
+      ...photo,
+      name: randomUUID() + '.' + photo.mimeType.split('/')[1],
+    }));
+
+    for (const photo of photosWithNames) {
+      await this.fileService.upload(photo.content, photo.name, photo.mimeType);
+    }
+
+    const photosInDb = await this.prisma.projectPhotos.createMany({
+      data: photosWithNames.map((photo) => ({ projectId, name: photo.name })),
+    });
+
+    return photosInDb;
+  }
 }
