895e41c31d8b538049b395321dc5c3e43167bef6
Gino Burdea <burdea.gino@gmail.com>
2024-10-13 12:32:59 +0300

    test: add tests for upload s3 file service

--- src/common/files.service.spec.ts
+++ src/common/files.service.spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { LocalFileService } from './files.service';
+import { LocalFileService, S3FileService } from './files.service';
 import { Chance } from 'chance';
 import { exists, readFileSync } from 'fs-extra';
 import { resolve } from 'path';
+import { HeadObjectCommand } from '@aws-sdk/client-s3';
 
 describe('LocalFileService', () => {
   let service: LocalFileService;
@@ -26,6 +27,36 @@ describe('LocalFileService', () => {
 
       const fileExists = await exists(resolve('.temp/uploads', name));
       expect(fileExists).toEqual(true);
+    });
+  });
+});
+
+describe('S3FileService', () => {
+  let service: S3FileService;
+
+  beforeEach(async () => {
+    jest.resetAllMocks();
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [S3FileService],
+    }).compile();
+
+    service = module.get<S3FileService>(S3FileService);
+  });
+
+  describe('upload', () => {
+    it('Should upload a file to S3', async () => {
+      const file = readFileSync(resolve('test/data/photo.png'));
+      const name = new Chance().string({ length: 16 }) + '.png';
+
+      await service.upload(file, name, 'image/png');
+
+      const getObjectCommand = new HeadObjectCommand({
+        Bucket: process.env.S3_BUCKET,
+        Key: name,
+      });
+      // If the file does not exist, this will throw an error and the test will fail
+      await service['s3Client'].send(getObjectCommand);
     });
   });
 });
