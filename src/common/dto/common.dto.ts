880cd9be11da83fe037b9e0337a1a60d99ae9f81
Gino Burdea <burdea.gino@gmail.com>
2024-10-20 19:05:00 +0300

    feat: add api common responses for swagger

---
+++ src/common/dto/common.dto.ts
@@ -0,0 +1,17 @@
+import { IsObject, IsString } from 'class-validator';
+
+class BaseErrorResponse {
+  @IsString()
+  error: string;
+
+  @IsString()
+  message: string;
+}
+
+export class UnknownErrorResponse extends BaseErrorResponse {}
+
+export class ValidationErrorResponse extends BaseErrorResponse {
+  /** Cheia este argumentul cu eroare, iar valoarea acestuia este eroarea */
+  @IsObject()
+  details: object;
+}
